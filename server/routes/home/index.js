354d4c316f36e6352064435fec3bb9e47b42b3b7
Realetive <realetive@yandex.ru>
2018-03-11 00:42:43 +0300

    Update index page

--- server/routes/home/index.js
+++ server/routes/home/index.js
@@ -2,25 +2,25 @@
 
 const { normalize } = require( 'normalizr' );
 const getStructure = require( '../../queries/getStructure' );
-const getTours = require( '../../queries/getTours' );
 const getSetting = require( '../../queries/getSetting' );
+const getTour = require( '../../queries/getTour' );
 
 const data = async ( context, params ) => {
   const response = await Promise.all( [
     getStructure( { lang: params.lang } ),
-    getTours( { lang: params.lang } ),
     getSetting( { lang: params.lang } ),
+    getTour( { lang: params.lang } ),
   ] );
   return normalize(
     {
       structure: response[0],
-      tours: response[2],
       setting: response[1],
+      tour: response[2],
     },
     {
       // structure: { object: schema.structure },
-      // tours: { results: [ schema.settings ] },
       // setting: { results: [ schema.settings ] },
+      // tour: { results: [ schema.settings ] },
     },
   );
 };
