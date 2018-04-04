0b43e9c297bfed056fedb4cf8041974bde6443e8
Realetive <realetive@yandex.ru>
2018-04-04 19:30:33 +0300

    Separate order to tours and self-order forms

--- components/common.blocks/page-cart/page-cart.deps.js
+++ components/common.blocks/page-cart/page-cart.deps.js
@@ -6,12 +6,17 @@
       mods: { width: 'tiny' },
     },
     {
+      block: 'tour',
+      mods: { view: 'order' }
+    },
+    {
       block: 'form',
       mods: {
         view: 'cart',
         'has-validation': true,
         message: ['popup', 'text'],
       }
-    }
+    },
+    'layout'
   ]
 }]
