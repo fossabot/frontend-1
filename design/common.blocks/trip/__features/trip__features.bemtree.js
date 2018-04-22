aecff598ca3a13a46edb32bfa41d6ca16f4a50c9
Realetive <realetive@yandex.ru>
2018-04-22 19:06:24 +0300

    Remove unused block (`trip`)

--- design/common.blocks/trip/__features/trip__features.bemtree.js
+++
@@ -1,9 +0,0 @@
-block( 'trip' ).elem( 'features' )(
-  replace()( ( node, ctx ) => {
-    return {
-      block: 'list',
-      mods: { type: 'circle' },
-      items: ctx.content,
-    };
-  } ),
-);
--- design/common.blocks/trip/__heading/trip__heading.deps.js
+++
@@ -1,6 +0,0 @@
-[{
-  shouldDeps: [
-    { block: 'heading', mods: { size: 'l' } },
-    { block: 'page', elem: 'heading', mod: 'size', val: 'l' }
-  ]
-}]
