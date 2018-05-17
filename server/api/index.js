c79452a24da841ea567e1bdbe10f51bec227cf1a
Realetive <realetive@yandex.ru>
2018-05-17 12:06:27 +0300

    astraMarin API

--- server/api/index.js
+++ server/api/index.js
@@ -8,10 +8,16 @@ const action = async ( { query }, { service, id } ) => {
 
   switch ( service ) {
     case 'nevatravel':
-      api = new nevaTravel( process.env.NEVATRAVEL_HOST, process.env.NEVATRAVEL_KEY );
+      api = new nevaTravel(
+        process.env.NEVATRAVEL_HOST,
+        process.env.NEVATRAVEL_KEY );
       break;
     case 'astramarin':
-      api = new astraMarin( process.env.ASTRAMARIN_HOST, process.env.ASTRAMARIN_LOGIN, process.env.ASTRAMARIN_PASSWORD );
+      api = new astraMarin(
+        process.env.ASTRAMARIN_HOST,
+        process.env.ASTRAMARIN_LOGIN,
+        process.env.ASTRAMARIN_PASSWORD,
+        process.env.ASTRAMARIN_EMAIL );
       break;
     default:
       throw new Error( 'Service is not defined' );
@@ -24,9 +30,28 @@ const action = async ( { query }, { service, id } ) => {
   // ] );
 
   // Astra
-  // const init = await api.init();
-  // console.log( init );
+  await api._GetServiceGroup();
 
+  const today = new Date();
+  const todayPlusMonth = new Date( today.setMonth( today.getMonth() + 1 ) );
+  await api._ServicesOnDate( today, todayPlusMonth, 'Т00000002' );
+  const EventsOnDate = await api._EventsOnDate( today, '000000001' ); // 000000001 → "City Sightseeing NEVA"
+
+  api.venueID = EventsOnDate._EventsOnDate[ EventsOnDate._EventsOnDate.length - 1 ].VenueID;
+  api.eventID = EventsOnDate._EventsOnDate[ EventsOnDate._EventsOnDate.length - 1 ].ID;
+
+  await api._SeatСategory();
+
+  await api._SeatsOnEvent( '000033078' );
+  // await api._BookingSeat( 'PALMIRA-3', 'nevatrip_testapi_000001', '000033078' );
+  // await api._BookingSeat( 'PALMIRA-3', 'nevatrip_testapi_000001', '000033078' );
+  await api._GetTicketType();
+  await api._GetPaymentType();
+  api.categoryID = '000000002';
+  await api._GetSeatPrice( '000033078', '000000001', '000000001', false, '', '000000002' );
+
+  const response = api;
+  console.log( response );
 
   if ( query.date ) {
     await api.date( query.date, { program: id, pier: query.pier } );
@@ -34,6 +59,7 @@ const action = async ( { query }, { service, id } ) => {
   
   // await api.pier();
   // await api.direction();
+
   // await api.time();
   // await api.tickets();
   // await api.seat();
--- server/api/provider/astraMarin.js
+++ server/api/provider/astraMarin.js
@@ -3,24 +3,305 @@
 const rest = require( 'rest' );
 const mime = require( 'rest/interceptor/mime' );
 const basicAuth = require( 'rest/interceptor/basicAuth' );
+const fastXmlParser = require( 'fast-xml-parser' );
 const api = rest
   .wrap( basicAuth )
   .wrap( mime );
 
 class astraMarin {
-  constructor( path, username, password ) {
+  constructor( path, username, password, email ) {
     this.path = () => path;
     this.username = () => username;
     this.password = () => password;
+    this.email = () => email;
+    this.trip = {};
   }
 
   async init() {
-    return await api( { path: this.path, username: this.username, password: this.password } ).then( response => {
-      console.log( response.entity );
+    return await api( {
+      path: this.path() + '?wsdl',
+      username: this.username(),
+      password: this.password()
+    } ).then( response => {
+      const jsonObj = fastXmlParser.parse( response.entity );
+      console.log( jsonObj );
       return this;
     } )
-
   }
+
+  static _createRequest( method, body ) {
+    return `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:int="http://localhost/InternetSaleJSON">
+       <soapenv:Header/>
+       <soapenv:Body>
+          <int:${ method }>
+            ${ body ? '<int:StringJSON>' + JSON.stringify( body ) + '</int:StringJSON>' : '' }
+          </int:${ method }>
+       </soapenv:Body>
+    </soapenv:Envelope>`;
+  }
+
+  // 
+  set serviceGroupID( serviceGroupID ) { this.trip.serviceGroupID = serviceGroupID; }
+  get serviceGroupID() { return this.trip.serviceGroupID; }
+  // 
+  set serviceID( serviceID ) { this.trip.serviceID = serviceID; }
+  get serviceID() { return this.trip.serviceID; }
+  // 
+  set eventID( eventID ) { this.trip.eventID = eventID; }
+  get eventID() { return this.trip.eventID; }
+  // 
+  set venueID( venueID ) { this.trip.venueID = venueID; }
+  get venueID() { return this.trip.venueID; }
+  // 
+  set categoryID( categoryID ) { this.trip.categoryID = categoryID; }
+  get categoryID() { return this.trip.categoryID; }
+  // 
+  set seatID( seatID ) { this.trip.seatID = seatID; }
+  get seatID() { return this.trip.seatID; }
+  // 
+  set sessionID( sessionID ) { this.trip.sessionID = sessionID; }
+  get sessionID() { return this.trip.sessionID; }
+  // 
+  set ticketTypeID( ticketTypeID ) { this.trip.ticketTypeID = ticketTypeID; }
+  get ticketTypeID() { return this.trip.ticketTypeID; }
+  // 
+  set paymentTypeID( paymentTypeID ) { this.trip.paymentTypeID = paymentTypeID; }
+  get paymentTypeID() { return this.trip.paymentTypeID; }
+  // 
+  set resident( resident ) { this.trip.resident = resident; }
+  get resident() { return this.trip.resident; }
+
+
+  // 1
+  async _GetServiceGroup() {
+    return await api( {
+      path     : this.path(),
+      username : this.username(),
+      password : this.password(),
+      entity   : this._createRequest( 'GetServiceGroup' ),
+    } ).then( response => {
+      const jsonObj = fastXmlParser.parse( response.entity );
+      this._GetServiceGroup = JSON.parse( jsonObj['soap:Envelope']['soap:Body']['m:GetServiceGroupResponse']['m:return']['d4p1:ServiceType']['d4p1:StringJSON'] );
+
+      return this;
+    } )
+  }
+
+  // 2
+  async _ServicesOnDate( dateFrom, dateTo = '', serviceGroupID = '' ) {
+    this.trip.serviceGroupID = this.trip.serviceGroupID || serviceGroupID;
+    const request = {
+      Date_From       : dateFrom.toISOString(),
+      Date_To         : dateTo.toISOString(),
+      ServiceGroup_ID : this.trip.serviceGroupID,
+      Email           : this.email(),
+    };
+
+    return await api( {
+      path     : this.path(),
+      username : this.username(),
+      password : this.password(),
+      entity   : this._createRequest( 'ServicesOnDate', request ),
+    } ).then( response => {
+      const jsonObj = fastXmlParser.parse( response.entity );
+      this._ServicesOnDate = JSON.parse( jsonObj['soap:Envelope']['soap:Body']['m:ServicesOnDateResponse']['m:return']['d4p1:Services']['d4p1:StringJSON'] );
+
+      return this;
+    } )
+  }
+
+  // 3
+  async _EventsOnDate( date, serviceID, serviceGroupID, eventID = '' ) {
+    this.trip.serviceID = this.trip.serviceID || serviceID;
+    this.trip.serviceGroupID = this.trip.serviceGroupID || serviceGroupID;
+    this.trip.eventID = this.trip.eventID || eventID;
+    const request = {
+      Service_ID      : this.trip.serviceID,
+      Date            : date.toISOString(),
+      ServiceGroup_ID : this.trip.serviceGroupID,
+      Event_ID        : this.trip.eventID,
+      Email           : this.email(),
+    };
+
+    return await api( {
+      path     : this.path(),
+      username : this.username(),
+      password : this.password(),
+      entity   : this._createRequest( 'EventsOnDate', request ),
+    } ).then( response => {
+      const jsonObj = fastXmlParser.parse( response.entity );
+      this._EventsOnDate = JSON.parse( jsonObj['soap:Envelope']['soap:Body']['m:EventsOnDateResponse']['m:return']['d4p1:Event']['d4p1:StringJSON'] );
+
+      return this;
+    } )
+  }
+
+  // 4
+  async _SeatСategory( venueID ) {
+    this.trip.venueID = this.trip.venueID || venueID;
+    const request = {
+      Venue_ID : this.trip.venueID,
+      Email    : this.email(),
+    };
+
+    return await api( {
+      path     : this.path(),
+      username : this.username(),
+      password : this.password(),
+      entity   : this._createRequest( 'SeatСategory', request ),
+    } ).then( response => {
+      const jsonObj = fastXmlParser.parse( response.entity );
+      this._SeatСategory = JSON.parse( jsonObj['soap:Envelope']['soap:Body']['m:Seat']['m:return']['d4p1:Seats']['d4p1:StringJSON'] );
+
+      return this;
+    } )
+  }
+
+  // 5
+  async _SeatsOnEvent( eventID, categoryID = '' ) {
+    this.trip.eventID = this.trip.eventID || eventID;
+    this.trip.categoryID = this.trip.categoryID || categoryID;
+    const request = {
+      Event_ID    : this.trip.eventID,
+      Category_ID : this.trip.categoryID,
+      Email       : this.email(),
+    };
+
+    return await api( {
+      path     : this.path(),
+      username : this.username(),
+      password : this.password(),
+      entity   : this._createRequest( 'SeatsOnEvent', request ),
+    } ).then( response => {
+      const jsonObj = fastXmlParser.parse( response.entity );
+      this._SeatsOnEvent = JSON.parse( jsonObj['soap:Envelope']['soap:Body']['m:SeatsOnEventResponse']['m:return']['d4p1:Seats']['d4p1:StringJSON'] );
+
+      return this;
+    } )
+  }
+
+  // 6
+  async _BookingSeat( seatID, sessionID, eventID ) {
+    this.trip.seatID = this.trip.seatID || seatID;
+    this.trip.sessionID = this.trip.sessionID || sessionID;
+    this.trip.eventID = this.trip.eventID || eventID;
+    const request = {
+      Event_ID   : this.trip.eventID,
+      Seat_ID    : this.trip.seatID,
+      Session_ID : this.trip.sessionID,
+      Email      : this.email(),
+    };
+
+    return await api( {
+      path     : this.path(),
+      username : this.username(),
+      password : this.password(),
+      entity   : this._createRequest( 'BookingSeat', request ),
+    } ).then( response => {
+      const jsonObj = fastXmlParser.parse( response.entity );
+      this._BookingSeat = JSON.parse( jsonObj['soap:Envelope']['soap:Body']['m:BookingSeatResponse']['m:return']['d4p1:Book']['d4p1:StringJSON'] );
+
+      return this;
+    } )
+  }
+
+  // 7
+  async _CancelBookingSeat( seatID = '', sessionID, eventID ) {
+    this.trip.seatID = this.trip.seatID || seatID;
+    this.trip.sessionID = this.trip.sessionID || sessionID;
+    this.trip.eventID = this.trip.eventID || eventID;
+    const request = {
+      Event_ID   : this.trip.eventID,
+      Seat_ID    : this.trip.seatID,
+      Session_ID : this.trip.sessionID,
+      Email      : this.email(),
+    };
+
+    return await api( {
+      path     : this.path(),
+      username : this.username(),
+      password : this.password(),
+      entity   : this._createRequest( 'CancelBookingSeat', request ),
+    } ).then( response => {
+      const jsonObj = fastXmlParser.parse( response.entity );
+      this._CancelBookingSeat = JSON.parse( jsonObj['soap:Envelope']['soap:Body']['m:CancelBookingSeatResponse']['m:return']['d4p1:CancelBook']['d4p1:StringJSON'] );
+
+      return this;
+    } )
+  }
+
+  // 8
+  async _GetTicketType( serviceID ) {
+    this.trip.serviceID = this.trip.serviceID || serviceID;
+    const request = {
+      Service_ID : this.trip.serviceID,
+      Email      : this.email(),
+    };
+
+    return await api( {
+      path     : this.path(),
+      username : this.username(),
+      password : this.password(),
+      entity   : this._createRequest( 'GetTicketType', request ),
+    } ).then( response => {
+      const jsonObj = fastXmlParser.parse( response.entity );
+      this._GetTicketType = JSON.parse( jsonObj['soap:Envelope']['soap:Body']['m:GetTicketTypeResponse']['m:return']['d4p1:TicketType']['d4p1:StringJSON'] );
+
+      return this;
+    } )
+  }
+
+  // 9
+  async _GetPaymentType() {
+    const request = {
+      Email      : this.email(),
+    };
+
+    return await api( {
+      path     : this.path(),
+      username : this.username(),
+      password : this.password(),
+      entity   : this._createRequest( 'GetPaymentType', request ),
+    } ).then( response => {
+      const jsonObj = fastXmlParser.parse( response.entity );
+      console.log( jsonObj['soap:Envelope']['soap:Body'] );
+      this._GetPaymentType = JSON.parse( jsonObj['soap:Envelope']['soap:Body']['m:GetPaymentTypeResponse']['m:return']['d4p1:PaymentType']['d4p1:StringJSON'] );
+      console.log( this._GetPaymentType );
+
+      return this;
+    } )
+  }
+  
+  // 10
+  async _GetSeatPrice( eventID, ticketTypeID, paymentTypeID, resident = false, seatID = '', categoryID = '' ) {
+    this.trip.eventID = this.trip.eventID || eventID;
+    this.trip.ticketTypeID = this.trip.ticketTypeID || ticketTypeID;
+    this.trip.paymentTypeID = this.trip.paymentTypeID || paymentTypeID;    
+    this.trip.resident = this.trip.resident || resident;    
+    this.trip.seatID = this.trip.seatID || seatID;
+    this.trip.categoryID = this.trip.categoryID || categoryID;
+    const request = {
+      Event_ID       : this.trip.eventID,
+      Seat_ID        : this.trip.seatID,
+      Category_ID    : this.trip.categoryID,
+      TicketType_ID  : this.trip.ticketTypeID,
+      PaymentType_ID : this.trip.paymentTypeID,
+      Email          : this.email(),
+    };
+
+    return await api( {
+      path     : this.path(),
+      username : this.username(),
+      password : this.password(),
+      entity   : this._createRequest( 'GetSeatPrice', request ),
+    } ).then( response => {
+      const jsonObj = fastXmlParser.parse( response.entity );
+      this._GetSeatPrice = JSON.parse( jsonObj['soap:Envelope']['soap:Body']['m:GetSeatPriceResponse']['m:return']['d4p1:Price']['d4p1:StringJSON'] );
+
+      return this;
+    } )
+  }
+  
 }
 
 module.exports = astraMarin;
